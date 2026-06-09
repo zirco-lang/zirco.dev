90e193dbd153f9c2f37f2cf6a5ed009a87755df1
Logan Devine <nutdriver716@gmail.com>
2026-06-08 21:21:11 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ export function Hero() {
                     <div className="mb-6 md:mb-8">
                         <img
                             src={zircoLogoLight}
-                            alt="Zirco Programming Language [test]"
+                            alt="Zirco Programming Language"
                             className="h-16 md:h-20 w-auto dark:hidden"
                         />
                         <img
